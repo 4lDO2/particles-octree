7d62a88d1d37b7842011d150bb6878490571bb91
4lDO2 <4lDO2@protonmail.com>
2024-12-14 12:24:32 +0100

    Almost finish tree.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,66 @@ use std::io::{BufRead, BufReader};
 use std::time::Instant;
 
 use anyhow::{Context, Result};
+use nalgebra::Vector3;
 
-fn square(v: [f32; 3]) -> f32 {
-    v[0] * v[0] + v[1] * v[1] + v[2] * v[2]
+enum Node<'arena> {
+    Split { children: [Option<&'arena mut Node<'arena>>; 8] },
+    Final { particle_idx: u32 },
 }
-fn subtract(a: [f32; 3], b: [f32; 3]) -> [f32; 3] {
-    [a[0] - b[0], a[1] - b[1], a[2] - b[2]]
+struct Tree<'arena> {
+    root: Option<&'arena mut Node<'arena>>,
+    root_level: i8,
+}
+
+const RADIUS: f32 = 0.05;
+
+fn vector_to_idx(vector: &Vectorf) -> u8 {
+    u8::from(vector.x >= 0.0) | (u8::from(vector.y >= 0.0) << 1) | (u8::from(vector.z >= 0.0) << 2)
+}
+
+impl<'arena> Tree<'arena> {
+    pub fn insert(&'arena mut self, mut pos_radius_relative: &Vectorf, index: u32, arena: &'arena mut bumpalo::Bump) {
+        if self.root.is_none() || pos_radius_relative.magnitude_squared() >= 3.0 * f32::powi(2.0, self.root_level.into()) {
+            let mut children = [const { None }; 8];
+            children[usize::from(vector_to_idx(pos_radius_relative))] = Some(self.root.take());
+            let parent = arena.alloc::<Node<'arena>>(Node::Split { children: [const { None }; 8] });
+            self.root = Some(parent);
+            self.root_level += 1;
+        } else {
+            self.root.as_mut().unwrap().insert(pos_radius_relative, index, self.root_level, arena);
+        }
+    }
+}
+impl<'arena> Node<'arena> {
+    pub fn insert(&'arena mut self, mut pos_radius_relative: &Vectorf, index: u32, level: i8, arena: &'arena mut bumpalo::Bump) {
+        match *self {
+            Self::Final { particle_idx } => if index != particle_idx {
+                todo!("new node");
+            }
+            Self::Split { ref mut children, .. } => {
+                let aabb_size = f32::powi(2.0, level.into());
+                let child_idx = usize::from(vector_to_idx(&pos_radius_relative));
+                let child = &mut children[child_idx];
+
+                if let Some(child) = child {
+                    let direction = Vectorf::new(
+                        if child_idx & 1 == 1 { 1.0 } else { -1.0 },
+                        if child_idx & 2 == 2 { 1.0 } else { -1.0 },
+                        if child_idx & 4 == 4 { 1.0 } else { -1.0 },
+                    );
+                    child.insert(&(pos_radius_relative + direction * aabb_size), index, level - 1, arena)
+                } else {
+                    *child = Some(arena.alloc::<Node<'arena>>(Node::Final { particle_idx: index }));
+                }
+            }
+        }
+    }
+}
+
+type Vectorf = Vector3<f32>;
+
+fn square(v: Vectorf) -> f32 {
+    v.dot(&v)
 }
 
 fn main() -> Result<()> {
@@ -18,30 +72,62 @@ fn main() -> Result<()> {
             .context("expected input file as argument")?,
     )?);
 
-    let rows = {
-        file.lines().map(|line| -> Result<[f32; 3]> {
+    let particles = {
+        file.lines().map(|line| -> Result<Vectorf> {
             let line = line?;
             let mut words = line.split(' ').map(|w| w.parse::<f32>());
-            Ok([
+            Ok(Vectorf::new(
                 words.next().context("1st not present")??,
                 words.next().context("2nd not present")??,
                 words.next().context("3rd not present")??,
-            ])
-        })
+            ))
+        }).collect::<Result<Vec<_>>>()?
     };
 
-    let threshold = 0.05;
+    let max_depth = {
+        // Calculate size of the smallest cube that fits all points (without translating or
+        // rotating the data).
 
-    let particles = rows.collect::<Result<Vec<_>>>()?;
-    let mut pairs = Vec::with_capacity(particles.len() * (particles.len() - 1) / 2);
+        let mut max = Vectorf::zeros();
+        let mut min = Vectorf::zeros();
+        for particle in &particles {
+            for coord in 0..3 {
+                if particle[coord] > max[coord] {
+                    max[coord] = particle[coord] / RADIUS;
+                } else if particle[coord] < max[coord] {
+                    min[coord] = particle[coord] / RADIUS;
+                }
+            }
+        }
+        let min = min.map(|m| (m.floor() as i32).min(0));
+        let max = max.map(|m| (m.ceil() as i32).max(0));
+        println!("Bounds: {} {} {}, {} {} {}", min[0], min[1], min[2], max[0], max[1], max[2]);
+
+        max.iter().copied().chain(min.iter().copied())
+            .map(|coord| coord.abs() as u32)
+            .max().unwrap_or(0).next_power_of_two().trailing_zeros() + 1
+
+    };
+    println!("Tree max depth: {max_depth}");
+
+    let mut tree = Tree {
+        root: None,
+        root_level: 0,
+    };
+    let mut arena = bumpalo::Bump::new();
+    for (i, pos) in particles.iter().enumerate() {
+        tree.insert(&(pos / RADIUS), i as u32, &mut arena);
+    }
+
     println!("Input: {} coordinates", particles.len());
+    let mut pairs = Vec::with_capacity(1024 * 1024 * 1024);
 
     let now = Instant::now();
 
     for i in 0..particles.len() {
         for j in i + 1..particles.len() {
-            if square(subtract(particles[i], particles[j])) <= threshold * threshold {
-                pairs.push((i, j));
+            if square(particles[i] - particles[j]) <= RADIUS * RADIUS {
+                pairs.push((i as u32, j as u32));
             }
         }
     }
